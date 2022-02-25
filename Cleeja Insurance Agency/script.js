6fe1b3d037e71bfdd5188411da15d47b9dc5b41c
Finey Otieno <70695768+Finey254@users.noreply.github.com>
2022-02-25 20:01:04 +0300

    Add files via upload

---
+++ Cleeja Insurance Agency/script.js
@@ -0,0 +1,80 @@
+let formBtn = document.querySelector('#form-button');
+let loginForm = document.querySelector('.login-form-container');
+let loginformClose = document.querySelector('#login-form-close');
+let claimBtn= document.querySelector('#claim-btn');
+let claimForm= document.querySelector('.claims-form-container');
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 5000);
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    claimForm.classList.remove('active');
+};
+
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+loginformClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+claimBtn.addEventListener('click', () =>{
+    claimForm.classList.add('active');
+});
+
+
+
+
